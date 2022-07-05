2d55d9f8ce7e3db0ed9bdafab51e7ffa0caf2f88
lw <1109160884@qq.com>
2022-07-05 14:40:42 +0800

    路由初始化

---
+++ src/components/Login.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="login">
+    <h1>{{msg}}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+    name: "Login",
+    data () {
+        return {
+            msg:"登录组件"
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+h1{
+  color: red;
+}
+</style>
